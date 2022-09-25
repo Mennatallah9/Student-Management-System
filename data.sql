a595ae4a7b62c0add37a6f2f282a18cb801672ba
Mennatallah Ashraf <72235546+Mennatallah9@users.noreply.github.com>
2022-09-25 14:30:56 +0200

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `students` (
+  `fname` varchar(45) NULL,
+  `lname` varchar(45) NULL,
+  `stuID` varchar(12) NOT NULL,
+  `phone` varchar(45) NULL,
+  `address` varchar(45) NOT NULL,
+   PRIMARY KEY (`stuID`)
+);
+
+INSERT INTO `students` (`fname`, `lname`, `stuID`, `phone`, `address`) VALUES
+('Menna', 'Ashraf', '222333', '011111111', 'Alexandria, Egypt'),
+('Aya', 'Ahmed', '222334', '016653534', 'Cairo, Egypt'),
+('Sara', 'Mohamed', '222335', '027672546', 'Cairo, Egypt'),
+('Rana', 'Ahmed', '222336', '018778757', 'Giza, Egypt'),
+('Mennatallah', 'Mostafa', '222339', '012345678', 'Luxor, Egypt'),
